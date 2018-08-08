895fa56f49ce6852ecfd20e7d497f40d47b24faa
Angelini <lllouis@yahoo.com>
2018-08-08 13:49:46 +0100

    Create auth controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,44 +1,26 @@
-'use strict'
+'use strict';
 
-var models = require('../models')
-var fs = require( 'fs')
-
-
-
-
+let models = require('../models')
 
 
 module.exports.controller = function( app, strategy ) {
 
-	app.get('/user', strategy.authenticate(), function( req, res ) {
+	app.get('/user', function( req, res ) {
 		console.log( '/user user_controller')
-		// console.log( req.query)
-		// var params = { 
-		//   Bucket: config.Bucket,
-		//   Prefix: 'stuff/'
-		// }
-		// s3.listObjects( params, function( err, resp ) {
-		// 	if ( err ) {
-		// 		winston.debug( err )
-		// 		res.json( err )
-		// 	} else {
-		// 		winston.debug( resp )
-		// 		res.json( resp )
-		// 	}
-			
+        
+        res.json( 'ok' )
+
+		// winston.debug( "User " + req.user )
+		// res.json( req.user )
+		// return
+
+		// models.User.findOne({
+		// 	where: { id: 1 }, include: [ {model: models.Photo, as: 'photos' }]
+		// }).then( user => {
+		// 	res.json( user )
+		// }).catch( function( err ) {
+		// 	res.staus( 500 ).json( err )
 		// })
-
-		winston.debug( "User " + req.user )
-		res.json( req.user )
-		return
-
-		models.User.findOne({
-			where: { id: 1 }, include: [ {model: models.Photo, as: 'photos' }]
-		}).then( user => {
-			res.json( user )
-		}).catch( function( err ) {
-			res.staus( 500 ).json( err )
-		})
 		
 	})
 
