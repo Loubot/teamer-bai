ff2b960a848a509b01610614d06f9f3226cbc3c7
Angelini <lllouis@yahoo.com>
2018-08-08 11:59:39 +0100

    Get configs working

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+var models = require('./models');
+var fs = require( 'fs' );
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+app.use(serveStatic(__dirname + "/dist"));
+
+
+// Initialise passport
+var passport = require("passport");
+app.use( passport.initialize() );
+let strategy = require('./config/strategy')( passport );
+
+var models = require('./models');
+
+
+/* Include all express controllers */
+fs.readdirSync('./controllers').forEach(function (file) {
+    if(file.substr(-3) == '.js') {
+        var route = require('./controllers/' + file);
+        route.controller( app, strategy );
+        // route.controller( app, jwt, strategy );
+    }
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
