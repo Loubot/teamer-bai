eface09b6fb8b109a163e1472c3cdb43fe5b0fe7
Angelini <lllouis@yahoo.com>
2018-10-03 16:05:19 +0100

    Create message helper

---
+++ helpers/message_sender.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let winston = require('../config/winston-config').load_winston()
+
+module.exports = {
+    send_mail: function(data) {
+        winston.debug('Attempting to send mail')
+        winston.debug(data)
+
+        let SibApiV3Sdk = require('sib-api-v3-sdk');
+        let defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+        // Configure API key authorization: api-key
+        let apiKey = defaultClient.authentications['api-key'];
+        apiKey.apiKey = process.env.send_in_blue
+        // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+        //apiKey.apiKeyPrefix = 'Token';
+
+        let apiInstance = new SibApiV3Sdk.SMTPApi();
+
+        var options = {
+            method: 'POST',
+            url: 'https://api.sendinblue.com/v3/smtp/email',
+            tags: ['ddd'],
+            sender: {
+                name: 'Louis',
+                email: 'louisangelini@gmail.com'
+            },
+            to: [{
+                email: 'lllouis@yahoo.com',
+                name: 'Aidan'
+            }],
+            textContent: 'Hello Bollux',
+            replyTo: {
+                email: 'louisangelini@gmail.com'
+            },
+            subject: 'Hello'
+        }
+
+        // apiInstance.sendTransacEmail(options).then(function(data) {
+        //   console.log('API called successfully. Returned data: ' + data);
+        // }, function(error) {
+        //   console.error(error);
+        // });
+    }
+}
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+let express = require('express');
+let path = require('path');
+let serveStatic = require('serve-static');
 app = express();
-// var winston = require('./config/winston-config').load_winston()
-var models = require('./models');
-var fs = require( 'fs' );
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+// let winston = require('./config/winston-config').load_winston()
+let models = require('./models');
+let fs = require( 'fs' );
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
 app.use(serveStatic(__dirname + "/dist"));
 
 app.use( bodyParser.urlencoded ({
@@ -14,56 +14,17 @@ app.use( bodyParser.urlencoded ({
 }));
 app.use(bodyParser.json())
 
-var SibApiV3Sdk = require('sib-api-v3-sdk');
-
-var defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-// Configure API key authorization: api-key
-var apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = "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"
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//apiKey.apiKeyPrefix['api-key'] = "Token"
-
-var api = new SibApiV3Sdk.AccountApi()
-api.getAccount().then(function(data) {
-  console.log('API called successfully. Returned data: ' + data);
-}, function(error) {
-  console.error(error);
-});
-
-
-var request = require("request");
-// console.log( request )
-var options = { method: 'POST',
-  url: 'https://api.sendinblue.com/v3/smtp/email',
-  headers: { 'api-key': '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' },
-  body: 
-   { tags: [ 'sss' ],
-     sender: { name: 'Louis', email: 'louisangelini@gmail.com' },
-     to: [ { email: 'lllouis@yahoo.com', name: 'Aidan' } ],
-     subject: 'Me balls',
-     replyTo: { email: 'louisangelini@gmail.com', name: 'Louis' },
-     textContent: 'Hell bollux' },
-  json: true };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
 
 // Initialise passport
-var passport = require("passport");
+let passport = require("passport");
 app.use( passport.initialize() );
 let strategy = require('./config/strategy')( passport );
-
-var models = require('./models');
 
 
 /* Include all express controllers */
 fs.readdirSync('./controllers').forEach(function (file) {
     if(file.substr(-3) == '.js') {
-        var route = require('./controllers/' + file);
+        let route = require('./controllers/' + file);
         route.controller( app, strategy );
         // route.controller( app, jwt, strategy );
     }
@@ -71,6 +32,6 @@ fs.readdirSync('./controllers').forEach(function (file) {
 
 
 
-var port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
