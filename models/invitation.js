38ba370a754b017b7e3ae8d1be1ce3f50c36b362
Angelini <lllouis@yahoo.com>
2018-11-28 16:21:54 +0000

    Add confirm to invitaion

--- models/invitation.js
+++ models/invitation.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var Invitation = sequelize.define('Invitation', {
     userId: DataTypes.INTEGER,
-    eventId: DataTypes.INTEGER
+    eventId: DataTypes.INTEGER,
+    confirm: DataTypes.BOOLEAN
   }, {});
   Invitation.associate = function(models) {
     // associations can be defined here
