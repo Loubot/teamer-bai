81c2bf8383022034d8975c92545871231a36cfa4
Angelini <llouis@yahoo.com>
2018-12-15 16:45:25 +0000

    Updating confirm manually

--- controllers/invitation_controller.js
+++ controllers/invitation_controller.js
@@ -2,80 +2,124 @@
 
 let winston = require('../config/winston-config').load_winston()
 let models = require('../models')
-let invitation_helper = require( '../helpers/invitation_helper' )
+let invitation_helper = require('../helpers/invitation_helper')
 
-module.exports.controller = function( app, strategy ) {
+module.exports.controller = function(app, strategy) {
 
-    app.get( '/invitations', strategy.authenticate(), function( req, res ) {
-        winston.debug( '/invitations invitation_controller' )
+    app.get('/invitations', strategy.authenticate(), function(req, res) {
+        winston.debug('/invitations invitation_controller')
         models.Invitation.findAll({
-            include: [{ all: true }]
-        }).then( invitations => {
-            winston.debug( 'got all invites' )
-            winston.debug( invitations )
-            res.json( invitations )
-        }).catch( err => {
-            winston.debug( 'Get all invites failed' )
-            winston.debug( err )
-            res.status( 500 ).json( err )
+            include: [{
+                all: true
+            }]
+        }).then(invitations => {
+            winston.debug('got all invites')
+            winston.debug(invitations)
+            res.json(invitations)
+        }).catch(err => {
+            winston.debug('Get all invites failed')
+            winston.debug(err)
+            res.status(500).json(err)
         })
     })
 
-    app.post( '/invitation/event/:id', strategy.authenticate(), function( req, res ) {
-        winston.debug( '/invitation/event/:id invitation_controller' )
-        winston.debug( req.params )
-        winston.debug( req.body )
+    app.post('/invitation/event/:id', strategy.authenticate(), function(req, res) {
+        winston.debug('/invitation/event/:id invitation_controller')
+        winston.debug(req.params)
+        winston.debug(req.body)
         models.User.findAll({
-            where: { id: req.body }
-        }).then( users => {
-            winston.debug( 'Found Users' )
-            winston.debug( users )
-            invitation_helper.create_invitation( users, req.params )
-            res.json( req.body )
-        }).catch( err => {
-            winston.debug( 'Find users failed' )
-            winston.debug( err )
-            res.status( 500 ).json( err )
+            where: {
+                id: req.body
+            }
+        }).then(users => {
+            winston.debug('Found Users')
+            winston.debug(users)
+            invitation_helper.create_invitation(users, req.params)
+            res.json(req.body)
+        }).catch(err => {
+            winston.debug('Find users failed')
+            winston.debug(err)
+            res.status(500).json(err)
         })
-        
-        
+
+
     })
 
-    app.post( '/invitation/add-event', strategy.authenticate(), function( req, res ) {
-        winston.debug( '/invitation/add-event invitation_controller' )
-        winston.debug( req.params )
-        winston.debug( req.body )
-        models.Event.findOne( { where: { id: 1 } } ).then( event => {
-            models.Invitation.findOne( { where: { id: 1 } } ).then( invite => {
-                winston.debug( 'Found invite' )
-                winston.debug( invite )
-                event.addInvitation( invite )
+    app.post('/invitation/add-event', strategy.authenticate(), function(req, res) {
+        winston.debug('/invitation/add-event invitation_controller')
+        winston.debug(req.params)
+        winston.debug(req.body)
+        models.Event.findOne({
+            where: {
+                id: 1
+            }
+        }).then(event => {
+            models.Invitation.findOne({
+                where: {
+                    id: 1
+                }
+            }).then(invite => {
+                winston.debug('Found invite')
+                winston.debug(invite)
+                event.addInvitation(invite)
             })
-            winston.debug( 'Found event' )
-            winston.debug( event )
-            res.json( event )
-        }).catch( err => {
-            winston.debug( 'Failed to find event' )
-            winston.debug( err )
-            res.status( 500 ).json( err )
+            winston.debug('Found event')
+            winston.debug(event)
+            res.json(event)
+        }).catch(err => {
+            winston.debug('Failed to find event')
+            winston.debug(err)
+            res.status(500).json(err)
         })
     })
 
-    app.get( '/invitations/user/:userId', strategy.authenticate(), function( req, res ) {
-        winston.debug( '/invitations/user/:userId invitation_controller' )
-        winston.debug( req.params )
-        winston.debug( req.body )
+    //Confirm attendance
+    app.put('/invitations/:id', strategy.authenticate(), function(req, res) {
+        winston.debug('/invitations/:id invitation_controller')
+        winston.debug(req.params)
+        winston.debug(req.body)
+        models.Invitation.update(
+                req.body, {
+                    returning: true,
+                    where: {
+                        id: req.params.id
+                    }
+                }
+            ).then(function( rows ) {
+                winston.debug('Invitation updated')
+                winston.debug( rows )
+                models.Invitation.findOne({ where: { id: req.params.id } } ).then( invite => {
+                    winston.debug( 'Found updated record' )
+                    winston.debug( invite )
+                    res.json( invite )
+                })
+            })
+            .catch(err => {
+                winston.debug('Failed to update invitaion')
+                winston.debug(err)
+                res.status(500).json(err)
+            })
+    })
+
+    app.get('/invitations/user/:userId', strategy.authenticate(), function(req, res) {
+        winston.debug('/invitations/user/:userId invitation_controller')
+        winston.debug(req.params)
+        winston.debug(req.body)
         models.Invitation.findAll({
-            where: { userId: req.params.userId },
-            include: [{ all: true }]
-        }).then( invites => {
-            winston.debug( 'Found invites by userId' )
-            winston.debug( invites )
-            res.json( invites )
-        }).catch( err => {
-            winston.debug( 'Failed to find invites by userId' )
-            winston.debug( err )
-            res.status( 500 ).json( err )
+            where: {
+                userId: req.params.userId
+            },
+            include: [{
+                all: true
+            }]
+        }).then(invites => {
+            winston.debug('Found invites by userId')
+            winston.debug(invites)
+            res.json(invites)
+        }).catch(err => {
+            winston.debug('Failed to find invites by userId')
+            winston.debug(err)
+            res.status(500).json(err)
         })
     })
 }
