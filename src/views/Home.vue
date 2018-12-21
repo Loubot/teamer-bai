083da39a40c23ffdd2f7760febaa5b89f42f204b
Angelini <lllouis@yahoo.com>
2018-12-21 05:47:16 +0000

    Check hostname

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -294,6 +294,7 @@
         }, //end of data
         mounted() {
             this.token = window.localStorage.getItem("token");
+            console.log( window.location.hostname )
             this.$http
                 .get(
                     "http://localhost:5000/users", {
