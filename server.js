0e95c76ff745257a19ece9811947f553d6094f33
Louis Angelini <lllouis@yahoo.com>
2019-01-09 04:35:08 +0000

    Change server

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use( bodyParser.urlencoded ({
 }));
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.options('/*', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
   });
 
 
