e5734c0afd4b9d0aa664016827db32b4599b7640
Louis Angelini <lllouis@yahoo.com>
2019-01-09 04:37:27 +0000

    That didn't work

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ let fs = require( 'fs' );
 let bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
 const history = require('connect-history-api-fallback')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
@@ -16,12 +22,7 @@ app.use( bodyParser.urlencoded ({
 }));
 app.use(bodyParser.json())
 
-app.options('/*', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-  });
+
 
 
 // Initialise passport
