368b32410c3fb142c28ba5d9466d955a74d7ed72
Angelini <lllouis@yahoo.com>
2019-01-10 05:48:32 +0000

    try it sure

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const history = require('connect-history-api-fallback')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, GET" )
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin,Authorization, XMLHttpRequest, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Accept");
     next();
   });
