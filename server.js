477796d3a7450217f32d83ec1019c39b79fdfddd
Angelini <lllouis@yahoo.com>
2019-01-10 05:31:12 +0000

    Free cors

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ let fs = require( 'fs' );
 let bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
 const history = require('connect-history-api-fallback')
-let cors = require('cors')
 
-app.options('*', cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
