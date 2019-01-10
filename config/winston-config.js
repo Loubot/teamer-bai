3ecb90614fba03bc7a0bd3c22d8bddb7b56fc828
Angelini <lllouis@yahoo.com>
2019-01-10 04:55:16 +0000

    Try console

--- config/winston-config.js
+++ config/winston-config.js
@@ -14,6 +14,7 @@ module.exports = {
               // - Write to all logs with level `info` and below to `combined.log` 
               // - Write all logs error (and below) to `error.log`.
               //
+              new (winston.transports.Console)({ level: 'debug'}),
               new winston.transports.File({ filename: 'logfile.log', level: 'debug' })
             ]
           });
