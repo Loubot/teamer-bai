cf0c0578db19f57ef57d62049f2d2e7b97e5cfb5
Angelini <lllouis@yahoo.com>
2019-01-10 05:25:16 +0000

    Remove star

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const history = require('connect-history-api-fallback')
 let cors = require('cors')
 
 var corsOptions = {
-    origin: 'http://localhost:8080/*',
+    origin: 'http://localhost:8080',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
